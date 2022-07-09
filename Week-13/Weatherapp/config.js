2b02dd283bdd9fe2c3342914cbf337d572029872
VivekHande16 <Vivekhande16sep@gmail.com>
2022-07-09 23:38:33 +0530

    added week 13 assignment

---
+++ Week-13/Weatherapp/config.js
@@ -0,0 +1,10 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/forecast?q=",
+        API_KEY: "0908013b3835fd6a8f9053521055ac93",
+        COUNT: 5,
+        ID: "524901"
+    }
+}
+
+module.exports = constants;
